9e0f7e1cd3520c6b618b9cf86e4db7799f9fab43
HeeZJee <hafeezghanchi927@gmail.com>
2019-11-29 21:28:26 +0500

    hi

---
+++ assignment_4/answer_1.py
@@ -0,0 +1,16 @@
+"""Use a dictionary to store information about a person you know. Store their first name, last name, age, and the city in which they live. You should have keys such as first_name, last_name, age, and city. Print each piece of information stored in your dictionary. Add a new key value pair about qualification then update the qualification value to high academic level then delete it.
+"""
+
+bio = { 
+"first_name":"Hafeez",
+"last_name": "Ghanchi",
+"age": "21",
+"city": "Karachi"}
+for x in bio.items():
+    print(x)
+print('\n')
+
+bio.update({"qualification":"High Academic Level"})
+for x in bio.items():
+    print(x)
+
